df347d8e15d3b2429b375ee4a3b726cb88fd8f03
Ben Liset <bliset@nerdery.com>
2013-05-29 14:03:05 -0500

    * Supports joining multiple channels at once

--- utilities/commandparser.cpp
+++ utilities/commandparser.cpp
@@ -7,7 +7,7 @@ CommandParser::CommandParser(Server *parent) : QObject(parent)
 
 }
 
-IrcCommand* CommandParser::parse(QString commandStr) {
+IrcCommand *CommandParser::parse(QString commandStr) {
     QRegExp commandRX("^/([a-zA-Z]+) (\\S+)");
     QRegExp channelRX("([#&][^\\x07\\x2C\\s]{,200})");
     int pos = commandRX.indexIn(commandStr);
@@ -16,13 +16,20 @@ IrcCommand* CommandParser::parse(QString commandStr) {
         // /JOIN
         if(commandString.compare("join", Qt::CaseInsensitive) == 0) {
             QString channelStr = commandRX.cap(2);
-            if (channelRX.indexIn(channelStr) > -1) {
-                return IrcCommand::createJoin(channelStr, NULL);
+            QStringList channels;
+            int pos = 0;
+            while ((pos = channelRX.indexIn(channelStr, pos)) != -1)
+            {
+                channels << channelRX.cap(1);
+                pos += channelRX.matchedLength();
+            }
+            if (channels.count() > 0) {
+                return IrcCommand::createJoin(channels);
             }
             // RFC says to leave all channels if 0
+            // Some servers support this, some don't, so we're doing this client side
             else if (channelStr == "0") {
                 getServer()->partAllChannels();
-                return NULL;
             }
         }
         // /PART
@@ -30,7 +37,7 @@ IrcCommand* CommandParser::parse(QString commandStr) {
             QString channelStr = commandRX.cap(2);
             if (channelRX.indexIn(channelStr) > -1) {
                 //TODO: Get Reason
-                return IrcCommand::createPart(channelStr, NULL);
+               return IrcCommand::createPart(channelStr, NULL);
             }
         }
     }
